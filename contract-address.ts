76c0e29c000295b27ae6df4d82f37ead33dedad3
Huynh Ba Huy <19127420@student.hcmus.edu.vn>
2022-12-27 14:36:08 +0700

    nft

--- contract-address.ts
+++ contract-address.ts
@@ -3,10 +3,16 @@ const contract = {
         authority: "0x9c0495e415664E1C0426198b1155a9019D93202d",
         treasury: "0x4ce7E3773118933f9D81D045c37D29d699C3c690",
         token: "0x84b9B910527Ad5C03A9Ca831909E21e236EA7b06", // link
-        paymentSystem: "0x311a54045E95d133D21aC12b436BAf379814c30d"
+        paymentSystem: "0x4dA562108e9c8309205a98923e1563B14ebc6882"
     }
 }
 const tokenTestNet = {
     link: "0x84b9B910527Ad5C03A9Ca831909E21e236EA7b06",
-    usdt: "0x337610d27c682e347c9cd60bd4b3b107c9d34ddd"
+    usdt: "0x337610d27c682e347c9cd60bd4b3b107c9d34ddd",
+    busd: "0x78867bbeef44f2326bf8ddd1941a4439382ef2a7"
+}
+const feedTestNet = {
+    link_usd: "0x1B329402Cb1825C6F30A0d92aB9E2862BE47333f",
+    usdt_usd: "0xEca2605f0BCF2BA5966372C99837b1F182d3D620",
+    busd_usd: "0x9331b55D9830EF609A2aBCfAc0FBCE050A52fdEa"
 }
--- contracts/NFT.sol
+++ contracts/NFT.sol
@@ -1,28 +1,20 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity 0.8.17;
-
-import "./internal-upgradeable/TransferableUpgradeable.sol";
+pragma solidity ^0.8.17;
 
 import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/token/ERC721/presets/ERC721PresetMinterPauserAutoIdUpgradeable.sol";
-import "@uniswap/v2-core/contracts/interfaces/IUniswapV2Pair.sol";
-import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
-import "@openzeppelin/contracts-upgradeable/token/ERC20/utils/SafeERC20Upgradeable.sol";
-import "@openzeppelin/contracts-upgradeable/utils/structs/EnumerableSetUpgradeable.sol";
-import "@openzeppelin/contracts-upgradeable/utils/math/SafeMathUpgradeable.sol";
 
-import {FixedPointMathLib} from "./libraries/FixedPointMathLib.sol";
+import "@openzeppelin/contracts-upgradeable/interfaces/IERC20Upgradeable.sol";
+
+import "@openzeppelin/contracts-upgradeable/token/ERC20/utils/SafeERC20Upgradeable.sol";
 
 contract NFTCollection is
     UUPSUpgradeable,
-    ERC721PresetMinterPauserAutoIdUpgradeable,
-    TransferableUpgradeable
+    ERC721PresetMinterPauserAutoIdUpgradeable
 {
     using StringsUpgradeable for uint256;
     using SafeERC20Upgradeable for IERC20Upgradeable;
-    using FixedPointMathLib for uint256;
-    using SafeMathUpgradeable for uint256;
 
     /// @dev value is equal to keccak256("NFTCollection_v1")
     bytes32 public constant VERSION =
@@ -40,29 +32,24 @@ contract NFTCollection is
     uint256 public maxSupply;
     uint256 public maxMintAmount;
 
-    IERC20Upgradeable public sellTokenContract;
-    /// @dev convert native token to USD price
-    AggregatorV3Interface public native2USD;
-    mapping(address => address) public tokenERC20ToFeeds;
+    IERC20Upgradeable public busdContract;
+
     mapping(address => bool) public whitelisted;
 
     function init(
         string calldata name_,
         string calldata symbol_,
         string calldata baseTokenURI_,
-        address sellTokenToUSDFeed_,
-        IERC20Upgradeable sellTokenContract_,
-        AggregatorV3Interface native2USD_
+        IERC20Upgradeable busdContract_
     ) external initializer {
         address sender = _msgSender();
-        native2USD = native2USD_;
-        tokenERC20ToFeeds[address(sellTokenContract_)] = sellTokenToUSDFeed_;
+
         owner = sender;
         cost = 300 ether;
         maxSupply = 10_000;
         maxMintAmount = 200;
         baseExtension = ".json";
-        sellTokenContract = sellTokenContract_;
+        busdContract = busdContract_;
         baseTokenURI = baseTokenURI_;
 
         __ERC721PresetMinterPauserAutoId_init(name_, symbol_, baseTokenURI_);
@@ -83,64 +70,21 @@ contract NFTCollection is
         address owner
     );
 
-    function updateSupportedTokenPayment(
-        IERC20Upgradeable _token,
-        AggregatorV3Interface _tokenToUSDFeed
-    ) external onlyRole(OPERATOR_ROLE) {
-        //TODO
-        tokenERC20ToFeeds[address(_token)] = address(_tokenToUSDFeed);
-    }
-
-    function updateSellToken(
-        IERC20Upgradeable _token,
-        AggregatorV3Interface _tokenToUSDFeed
-    ) external onlyRole(OPERATOR_ROLE) {
-        //TODO
-        sellTokenContract = _token;
-        tokenERC20ToFeeds[address(_token)] = address(_tokenToUSDFeed);
-    }
-
     // public
-    function mint(
-        address _to,
-        uint256 _mintAmount,
-        address _paymentToken
-    ) external whenNotPaused {
+    function mint(address _to, uint256 _mintAmount) external whenNotPaused {
         uint256 supply = totalSupply();
         require(_mintAmount > 0);
         require(_mintAmount <= maxMintAmount);
         require(supply + _mintAmount <= maxSupply);
-        require(
-            tokenERC20ToFeeds[_paymentToken] != address(0),
-            "NFTCollection: UNSUPPORTED_PAYMENT_TOKEN"
-        );
+
         address sender = _msgSender();
+
         if (!hasRole(MINTER_ROLE, sender)) {
             if (whitelisted[sender] != true) {
-                int256 paymentTokenToUsdPrice;
-                if (_paymentToken == address(0)) {
-                    (, paymentTokenToUsdPrice, , , ) = native2USD
-                        .latestRoundData();
-                } else {
-                    AggregatorV3Interface feed = AggregatorV3Interface(
-                        tokenERC20ToFeeds[_paymentToken]
-                    );
-                    (, paymentTokenToUsdPrice, , , ) = feed.latestRoundData();
-                }
-                AggregatorV3Interface feedSellToken = AggregatorV3Interface(
-                    tokenERC20ToFeeds[address(sellTokenContract)]
-                );
-                (, int256 sellTokenToUsdPrice, , , ) = feedSellToken
-                    .latestRoundData();
-                uint price = cost.mul(_mintAmount).mulDivDown(
-                    uint(paymentTokenToUsdPrice),
-                    uint(sellTokenToUsdPrice)
-                );
-                _safeTransferFrom(
-                    IERC20Upgradeable(_paymentToken),
+                busdContract.safeTransferFrom(
                     sender,
                     owner,
-                    price
+                    cost * _mintAmount
                 );
             }
         }
